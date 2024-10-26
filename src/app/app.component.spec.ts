51d29343d5f73716b9ab8ae61948481b703bcd08
JuliaSouzaG <julia.s.gomes@hotmail.com>
2024-10-25 21:21:26 -0400

    cadastro de pessoa

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,29 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AppComponent],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have the 'amigos-sem-fronteiras' title`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('amigos-sem-fronteiras');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, amigos-sem-fronteiras');
+  });
+});
---
+++ src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { CadastroPessoaComponent } from './components/cadastro-pessoa/cadastro-pessoa.component';
+
+export const routes: Routes = [
+  { path: 'cadastro-pessoa', component: CadastroPessoaComponent },
+];
