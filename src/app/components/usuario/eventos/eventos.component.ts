e95a92be65eb4b4c8b3d2e09cbff718a039663d0
Júlia de Souza Gomes <julia.s.gomes@hotmail.com>
2024-11-04 09:56:40 -0400

    tela eventos

--- src/app/components/usuario/eventos/eventos.component.ts
+++ src/app/components/usuario/eventos/eventos.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-eventos',
   standalone: true,
-  imports: [MatCardModule, CommonModule, MatButtonModule],
+  imports: [MatCardModule, CommonModule, MatButtonModule, MatIconModule],
   templateUrl: './eventos.component.html',
   styleUrl: './eventos.component.css'
 })
@@ -17,25 +18,82 @@ export class EventosComponent {
     {
       nome: "Doação",
       data: "16/12/2024",
+      endereco: "Rua tal tal, Bairo tal. Nº0",
       instituicao_org: {
         nome: "Senac",
         telefone: "92 982402547"
-      },
-      local: {
-        rua: "tal tal",
-        bairro: "Chapada",
       }
     },
     {
       nome: "Distribuição de sopa",
       data: "16/07/2024",
+      endereco: "Rua tal tal, Bairo tal. Nº0",
       instituicao_org: {
         nome: "Senac",
         telefone: "92 982402547"
-      },
-      local: {
-        rua: "tal tal",
-        bairro: "Chapada",
+      }
+    },
+    {
+      nome: "Distribuição de sopa",
+      data: "16/07/2024",
+      endereco: "Rua tal tal, Bairo tal. Nº0",
+      instituicao_org: {
+        nome: "Senac",
+        telefone: "92 982402547"
+      }
+    },
+    {
+      nome: "Distribuição de sopa",
+      data: "16/07/2024",
+      endereco: "Rua tal tal, Bairo tal. Nº0",
+      instituicao_org: {
+        nome: "Senac",
+        telefone: "92 982402547"
+      }
+    },
+    {
+      nome: "Distribuição de sopa",
+      data: "16/07/2024",
+      endereco: "Rua tal tal, Bairo tal. Nº0",
+      instituicao_org: {
+        nome: "Senac",
+        telefone: "92 982402547"
+      }
+    },
+    {
+      nome: "Distribuição de sopa",
+      data: "16/07/2024",
+      endereco: "Rua tal tal, Bairo tal. Nº0",
+      instituicao_org: {
+        nome: "Senac",
+        telefone: "92 982402547"
+      }
+    },
+    {
+      nome: "Distribuição de sopa",
+      data: "16/07/2024",
+      endereco: "Rua tal tal, Bairo tal. Nº0",
+      instituicao_org: {
+        nome: "Senac",
+        telefone: "92 982402547"
+      }
+    },
+    {
+      nome: "Distribuição de sopa",
+      data: "16/07/2024",
+      endereco: "Rua tal tal, Bairo tal. Nº0",
+      instituicao_org: {
+        nome: "Senac",
+        telefone: "92 982402547"
+      }
+    },
+    {
+      nome: "Distribuição de sopa",
+      data: "16/07/2024",
+      endereco: "Rua tal tal, Bairo tal. Nº0",
+      instituicao_org: {
+        nome: "Senac",
+        telefone: "92 982402547"
       }
     }
   ]
